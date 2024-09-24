75590f709ecb4423632c5f58fcf72e1d0aca0b2a
LopataPC <lopata29435@gmail.com>
2024-09-25 00:19:33 +0300

    aint working P1

--- SET1/P1/release.cpp
+++ SET1/P1/release.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 #include <stdexcept>
+#include <unordered_set>
 #include "list.h"
 
 List::List() 
 : _size(0)
-, head()
-, tail()
+, head(nullptr)
+, tail(nullptr)
 {}
 
 List::List(const List& other) 
 : _size(other._size)
+, head(nullptr)
+, tail(nullptr)
 {
-    head = new Node(other.head->value);
+    if (!_size) {
+        return; 
+    }
+
+    head = new Node(*other.head);
     Node* current = head;
-    
     Node* tmp = other.head->next;
+
     while (tmp) {
         Node* newNode = new Node(current, nullptr, tmp->value);
         current->next = newNode;
@@ -30,7 +37,9 @@ List::List(std::vector<int> array)
 , head(nullptr)
 , tail(nullptr)
 {
-    if (_size == 0) return;
+    if (_size == 0) {
+        return;
+    }
     
     head = new Node(array[0]);
     Node* current = head;
@@ -51,6 +60,9 @@ List::~List() {
       current = current->next;
       delete tmp;  
     }
+    _size = 0;
+    head = nullptr;
+    tail = nullptr;
 }
 
 int List::front() {
@@ -66,17 +78,19 @@ void List::push_back(int value) {
     Node* pb = new Node(tail, nullptr, value);
     tail->next = pb;
     tail = pb;
+    _size++;
 }
 
 void List::push_front(int value) {
     Node* pf = new Node(nullptr, head, value);
     head->prev = pf;
     head = pf;
+    _size++;
 }
 
 void List::insert(Node* pos, int value) {
     if (!pos) {
-        std::runtime_error("Incorrect position!");
+        throw std::runtime_error("Incorrect position!");
     }
     Node* tmp = head;
     while (tmp) {
@@ -84,39 +98,43 @@ void List::insert(Node* pos, int value) {
             Node* ins = new Node(pos, pos->next, value);
             pos->next = ins;
             ins->next->prev = ins;
+            _size++;
             return;
         }
         tmp = tmp->next;
     }
-    std::runtime_error("Incorrect position!");
+    throw std::runtime_error("Incorrect position!");
 }
 
 void List::pop_front() {
     if (!head->next) {
-        std::runtime_error("Deleting in empty list");
+        throw std::runtime_error("Incorrect position!");
     }
     Node* tmp = head;
     head = head->next;
     head->prev = nullptr;
+    _size--;
     delete tmp;
 }
 
 void List::pop_back() {
     if (!tail->prev) {
-        std::runtime_error("Deleting in empty list");
+        throw std::runtime_error("Incorrect position!");
     }
     Node* tmp = tail;
     tail = tail->prev;
     tail->next = nullptr;
+    _size--;
     delete tmp;
 }
 
 void List::erase(Node* pos) {
     if (!pos) {
-        std::runtime_error("Incorrect position!");
+        throw std::runtime_error("Incorrect position!");
     }
     pos->prev->next = pos->next;
     pos->next->prev = pos->prev;
+    _size--;
     delete pos;
 }
 
@@ -146,30 +164,96 @@ void List::reverse() {
 }
 
 void List::remove_duplicates() {
-    throw std::runtime_error("Not implemented!");
-    //zhopa
+    std::unordered_set<int> uniq;
+    Node* current = head;
+    while (current) {
+        if (uniq.find(current->value) != uniq.end()) {
+            Node* tmp = current;
+            current = current->next;
+            erase(tmp);
+            _size--;
+            continue;
+        }
+        current = current->next;
+    }
 }
 
 void List::replace(int old_value, int new_value) {
-    throw std::runtime_error("Not implemented!");
+    Node* current = head;
+    while (current) {
+        if (current->value == old_value) {
+            current->value = new_value;
+        }
+        current = current->next;
+    }
 }
 
 void List::merge(const List& other) {
-    throw std::runtime_error("Not implemented!");
+    Node* current = tail;
+    Node* curCppy = other.head;
+    while (curCppy) {
+        Node* new_node = new Node(current, nullptr, curCppy->value);
+        current->next = new_node;
+        current = new_node;
+        curCppy = curCppy->next;
+    }
+    tail = current;
+    _size += other._size;
 }
 
 bool List::check_cycle() const {
-    throw std::runtime_error("Not implemented!");
+    Node* current = head;
+    Node* prev = current->prev;
+    while (current) {
+        if (current->prev != prev) {
+            return true;
+        }
+        prev = current;
+        current = current->next;
+    }
+    return false;
 }
 
 size_t List::size() const {
-    throw std::runtime_error("Not implemented!");
+    return _size;
 }
 
 bool List::empty() const{
-    throw std::runtime_error("Not implemented!");
+    return _size;
 }
 
 void List::copy(const List& other) {
-    throw std::runtime_error("Not implemented!");
+    Node* current = head;
+    Node* curCppy = other.head;
+
+    while (curCppy && current) {
+        current->value = curCppy->value;
+        current = current->next;
+        curCppy = curCppy->next;
+    }
+    
+    if (curCppy || current) {
+        return;
+    }
+
+    if (!curCppy) {
+        tail = current->prev;
+        _size = other._size;
+        while (current) {
+            Node* tmp = current;
+            current = current->next;
+            delete tmp;
+        }
+    }
+
+    if(!current) {
+        current = current->prev;
+        while (curCppy) {
+            Node* new_node = new Node(current, nullptr, curCppy->value);
+            current->next = new_node;
+            current = new_node;
+            curCppy = curCppy->next;
+            _size++;
+        }
+    }
 }
