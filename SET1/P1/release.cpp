91a06adca0eacd1cd7d8862d278c75ef40d9140f
LopataPC <lopata29435@gmail.com>
2024-09-27 18:51:41 +0300

    P1 finished

--- SET1/P1/release.cpp
+++ SET1/P1/release.cpp
@@ -3,19 +3,19 @@
 #include <unordered_set>
 #include "list.h"
 
-List::List() 
+List::List()
 : _size(0)
 , head(nullptr)
 , tail(nullptr)
 {}
 
-List::List(const List& other) 
+List::List(const List& other)
 : _size(other._size)
 , head(nullptr)
 , tail(nullptr)
 {
     if (!_size) {
-        return; 
+        return;
     }
 
     head = new Node(*other.head);
@@ -32,7 +32,7 @@ List::List(const List& other)
     tail = current;
 }
 
-List::List(std::vector<int> array) 
+List::List(std::vector<int> array)
 : _size(array.size())
 , head(nullptr)
 , tail(nullptr)
@@ -40,7 +40,7 @@ List::List(std::vector<int> array)
     if (_size == 0) {
         return;
     }
-    
+
     head = new Node(array[0]);
     Node* current = head;
 
@@ -61,20 +61,19 @@ int List::front() {
     return head->value;
 }
 
-
 int List::back() {
     return tail->value;
 }
 
 void List::push_back(int value) {
     Node* pb = new Node(tail, nullptr, value);
-    
+
     if (tail != nullptr) {
         tail->next = pb;
     } else {
         head = pb;
     }
-    
+
     tail = pb;
     _size++;
 }
@@ -82,13 +81,13 @@ void List::push_back(int value) {
 
 void List::push_front(int value) {
     Node* pf = new Node(nullptr, head, value);
-    
+
     if (head != nullptr) {
         head->prev = pf;
     } else {
         tail = pf;
     }
-    
+
     head = pf;
     _size++;
 }
@@ -98,59 +97,119 @@ void List::insert(Node* pos, int value) {
     if (!pos) {
         throw std::runtime_error("Incorrect position!");
     }
-    Node* tmp = head;
-    while (tmp) {
-        if(tmp == pos) {
+    Node* current = head;
+    while (current) {
+        if(current == pos) {
             Node* ins = new Node(pos, pos->next, value);
             pos->next = ins;
             ins->next->prev = ins;
             _size++;
             return;
         }
-        tmp = tmp->next;
+        current = current->next;
     }
     throw std::runtime_error("Incorrect position!");
 }
 
 void List::pop_front() {
-    if (!head->next) {
-        throw std::runtime_error("Incorrect position!");
+    if (!head) {
+        throw std::runtime_error("Deleting in empty list");
     }
-    Node* tmp = head;
-    head = head->next;
-    head->prev = nullptr;
+    if (head->next) {
+        Node* tmp = head;
+        head = head->next;
+        head->prev = nullptr;
+        delete tmp;
+    } else {
+        delete head;
+        head = nullptr;
+        tail = nullptr;
+    }
     _size--;
-    delete tmp;
 }
 
 void List::pop_back() {
-    if (!tail->prev) {
-        throw std::runtime_error("Incorrect position!");
+    if (!tail) {
+        throw std::runtime_error("Deleting in empty list");
     }
-    Node* tmp = tail;
-    tail = tail->prev;
-    tail->next = nullptr;
+    if (tail->prev) {
+        Node* tmp = tail;
+        tail = tail->prev;
+        tail->next = nullptr;
+        delete tmp;
+    } else {
+        delete tail;
+        head = nullptr;
+        tail = nullptr;
+    }
     _size--;
-    delete tmp;
 }
 
 void List::erase(Node* pos) {
-    if (!pos) {
+    if (pos == nullptr) {
         throw std::runtime_error("Incorrect position!");
     }
-    pos->prev->next = pos->next;
-    pos->next->prev = pos->prev;
-    _size--;
-    delete pos;
+    Node *current = head;
+    while (current != nullptr)
+    {
+        if (current == pos)
+        {
+            if (pos->prev == nullptr) {
+                pop_front();
+                return;
+            }
+            if (pos->next == nullptr) {
+                pop_back();
+                return;
+            }
+            pos->prev->next = pos->next;
+            pos->next->prev = pos->prev;
+            delete pos;
+            _size--;
+            return;
+        }
+        current = current->next;
+    }
+    throw std::runtime_error("Incorrect position!");
 }
 
 void List::clear() {
+    Node* slow = head;
+    Node* fast = head;
+    bool hasCycle = false;
+
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) {
+            hasCycle = true;
+            break;
+        }
+    }
+
+    if (hasCycle) {
+        slow = head;
+        while (slow != fast) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        Node* cycleStart = slow;
+        Node* prev = nullptr;
+        do {
+            prev = fast;
+            fast = fast->next;
+        } while (fast != cycleStart);
+
+        prev->next = nullptr;
+    }
+
     Node* current = head;
     while (current) {
         Node* tmp = current;
         current = current->next;
-        delete tmp;  
+        delete tmp;
     }
+    
     head = nullptr;
     tail = nullptr;
     _size = 0;
@@ -177,9 +236,9 @@ void List::remove_duplicates() {
             Node* tmp = current;
             current = current->next;
             erase(tmp);
-            _size--;
             continue;
         }
+        uniq.insert(current->value);
         current = current->next;
     }
 }
@@ -225,41 +284,39 @@ size_t List::size() const {
 }
 
 bool List::empty() const{
-    return _size;
+    return !_size;
 }
 
 void List::copy(const List& other) {
-    Node* current = head;
-    Node* curCppy = other.head;
-
-    while (curCppy && current) {
-        current->value = curCppy->value;
-        current = current->next;
-        curCppy = curCppy->next;
-    }
-    
-    if (curCppy || current) {
+    clear();
+    _size = other._size;
+    if (!_size) {
         return;
     }
 
-    if (!curCppy) {
-        tail = current->prev;
-        _size = other._size;
-        while (current) {
-            Node* tmp = current;
-            current = current->next;
-            delete tmp;
-        }
+    head = new Node(*other.head);
+    Node* current = head;
+    Node* tmp = other.head->next;
+
+    while (tmp) {
+        Node* newNode = new Node(current, nullptr, tmp->value);
+        current->next = newNode;
+        current = newNode;
+        tmp = tmp->next;
     }
 
-    if (!current) {
-        current = current->prev;
-        while (curCppy) {
-            Node* new_node = new Node(current, nullptr, curCppy->value);
-            current->next = new_node;
-            current = new_node;
-            curCppy = curCppy->next;
-            _size++;
-        }
-    }
+    tail = current;
+
 }
+
+// int main() {
+//     std::vector<int> v = {1, 2, 3};
+//     std::vector<int> v1 = {};
+//     List a = List(v);
+//     List b = List(v1);
+//     a.copy(b);
+//     for (auto it = a.head; it != nullptr; it = it->next) {
+//         std::cout << it->value << " ";
+//     }
+//     std::cout << "\n" << a.size();
+// }        
